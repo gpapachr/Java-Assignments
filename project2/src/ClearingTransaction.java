13183da08a107c6edfe25eb533168a683543ef1a
gpapachr <giannis.papachristou@yahoo.gr>
2021-06-12 01:19:39 +0300

    Update clearing transaction constructors

--- project2/src/ClearingTransaction.java
+++ project2/src/ClearingTransaction.java
@@ -1,10 +1,17 @@
 public class ClearingTransaction extends Transaction{
     private final ExpenseType expenseType;
-    private final String type = "CLEARING";
+    private final String type;
 
     ClearingTransaction(Employee employee, double costToPay, ExpenseType expenseType){
         super(employee, costToPay);
         this.expenseType = expenseType;
+        this.type = "CLEARING";
+    }
+
+    ClearingTransaction(Employee employee, double costToPay){
+        super(employee, costToPay);
+        this.expenseType = null;
+        this.type = "FINAL_CLEARING";
     }
 
     @Override
